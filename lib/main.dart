26d0aabe0ee46d24377c53ebd5b3299534f640c8
RameshMailan <justrameshm@gmail.com>
2024-08-28 11:15:41 +0530

    basic one

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CalculatorScreen(),
+      home: const CalculatorScreen(),
     );
   }
 }
@@ -27,8 +27,130 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _displayText = "0";
+  String _previousText = "";
+  String _operation = "";
+
+  void _onButtonPressed(String text) {
+    setState(() {
+      if (text == "C") {
+        _displayText = "0";
+        _previousText = "";
+        _operation = "";
+      } else if (text == "=") {
+        _calculateResult();
+      } else if (text == "+" || text == "-" || text == "*" || text == "/") {
+        _previousText = _displayText;
+        _operation = text;
+        _displayText = "0";
+      } else {
+        if (_displayText == "0") {
+          _displayText = text;
+        } else {
+          _displayText += text;
+        }
+      }
+    });
+  }
+
+  void _calculateResult() {
+    double num1 = double.parse(_previousText);
+    double num2 = double.parse(_displayText);
+    double result;
+
+    switch (_operation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        result = 0;
+    }
+    _displayText = result.toString();
+    _previousText = "";
+    _operation = "";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter calculator"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.centerRight,
+              child: Text(
+                _displayText,
+                style:
+                    const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const Divider(),
+          Row(
+            children: [
+              _buildButton("7"),
+              _buildButton("8"),
+              _buildButton("9"),
+              _buildButton("/"),
+            ],
+          ),
+          Row(
+            children: [
+              _buildButton("4"),
+              _buildButton("5"),
+              _buildButton("6"),
+              _buildButton("8"),
+            ],
+          ),
+          Row(
+            children: [
+              _buildButton("1"),
+              _buildButton("2"),
+              _buildButton("3"),
+              _buildButton("_"),
+            ],
+          ),
+          Row(
+            children: [
+              _buildButton("C"),
+              _buildButton("0"),
+              _buildButton("="),
+              _buildButton("+"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String text) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => _onButtonPressed(text),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.all(24),
+          backgroundColor: Colors.blue,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
